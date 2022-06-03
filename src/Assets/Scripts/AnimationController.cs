c2c90a485c08c714d2cac5587cf940c602c4c4b4
SugarWind <g2127037@st.t-kougei.ac.jp>
2022-06-03 21:35:40 +0900

    6. アニメーション

---
+++ src/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController
+{
+    float _time = 0.0f;
+    float _inv_time_max = 1.0f;
+    const float DELTA_TIME_MAX = 1.0f;
+
+    public  void Set(float max_time)
+    {
+        Debug.Assert(0.0f < max_time);
+
+        _time = max_time;
+        _inv_time_max = 1.0f / max_time;
+    }
+
+    // Update is called once per frame
+    public bool Update(float delta_time)
+    {
+        if (DELTA_TIME_MAX < delta_time) delta_time = DELTA_TIME_MAX;
+
+        _time -= delta_time;
+
+        if(_time < 0.0f)
+        {
+            _time = 0.0f;
+            return false;
+        }
+
+        return true;
+    }
+
+    public float GetNormalized()
+    {
+        return _time * _inv_time_max;
+    }
+}
--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    const float TRANS_TIME = 0.05f;
+    const float ROT_TIME = 0.05f;
+
     enum RotState
     {
         Up = 0,
@@ -19,6 +22,11 @@ public class PlayerController : MonoBehaviour
 
     Vector2Int _position;
     RotState _rotate = RotState.Up;
+
+    AnimationController _animationController = new AnimationController();
+    Vector2Int _last_position;
+    RotState _last_rotate = RotState.Up;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -48,18 +56,23 @@ public class PlayerController : MonoBehaviour
         return true;
     }
 
+    void SetTransition(Vector2Int pos, RotState rot, float time)
+    {
+        _last_position = _position;
+        _last_rotate = _rotate;
+
+        _position = pos;
+        _rotate = rot;
+
+        _animationController.Set(time);
+    }
 
     private bool Translate(bool is_right)
     {
         Vector2Int pos = _position + (is_right ? Vector2Int.right : Vector2Int.left);
         if (!CanMove(pos,_rotate)) return false;
 
-
-        _position = pos;
-
-        _puyoControllers[0].SetPos(new Vector3((float)_position.x, (float)_position.y, 0.0f));
-        Vector2Int posChild = CalcChildPuyoPos(_position, _rotate);
-        _puyoControllers[1].SetPos(new Vector3((float)_position.x, (float)_position.y + 1.0f, 0.0f));
+        SetTransition(pos, _rotate, TRANS_TIME);
 
         return true;
     }
@@ -95,14 +108,7 @@ public class PlayerController : MonoBehaviour
         }
         if (!CanMove(pos, rot)) return false;
 
-        
-        _position = pos;
-        _rotate = rot;
-
-        _puyoControllers[0].SetPos(new Vector3((float)_position.x, (float)_position.y, 0.0f));
-        Vector2Int posChild = CalcChildPuyoPos(_position, _rotate);
-        _puyoControllers[1].SetPos(new Vector3((float)posChild.x, (float)posChild.y, 0.0f));
-
+        SetTransition(pos, rot, ROT_TIME);
         return true;
     }
 
@@ -131,7 +137,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Control()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -157,4 +163,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if(!_animationController.Update(Time.deltaTime))
+        {
+            Control();
+        }
+
+        float anim_rate = _animationController.GetNormalized();
+        _puyoControllers[0].SetPos(Interpolate(_position, RotState.Invalid, _last_position, RotState.Invalid, anim_rate));
+        _puyoControllers[1].SetPos(Interpolate(_position, _rotate, _last_position, _last_rotate, anim_rate));
+    }
+
+    static Vector3 Interpolate(Vector2Int pos, RotState rot, Vector2Int pos_last, RotState rot_last, float rate)
+    {
+       
+        Vector3 p = Vector3.Lerp(
+            new Vector3((float)pos.x, (float)pos.y, 0.0f),
+            new Vector3((float)pos_last.x, (float)pos_last.y, 0.0f), rate);
+
+        if (rot == RotState.Invalid) return p;
+
+       
+        float theta0 = 0.5f * Mathf.PI * (float)(int)rot;
+        float theta1 = 0.5f * Mathf.PI * (float)(int)rot_last;
+        float theta = theta1 - theta0;
+
+        
+        if (+Mathf.PI < theta) theta = theta - 2.0f * Mathf.PI;
+        if (theta < -Mathf.PI) theta = theta + 2.0f * Mathf.PI;
+
+        theta = theta0 + rate * theta;
+
+        return p + new Vector3(Mathf.Sin(theta), Mathf.Cos(theta), 0.0f);
+    }
 }
